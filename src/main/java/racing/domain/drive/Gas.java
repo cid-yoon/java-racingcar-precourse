299320fd63b12da98acb83d72c4409b283b6c702
cid-yoon <guildhunt@knou.ac.kr>
2021-05-03 21:41:00 +0900

    refact : 가스 객체 개선
    수치 반환 및 검증에서 객체 자체를 반환하도록 개선

--- src/main/java/racing/domain/drive/Gas.java
+++ src/main/java/racing/domain/drive/Gas.java
@@ -1,15 +1,19 @@
 package racing.domain.drive;
 
+/**
+ * 자동차의 연료
+ * 이동 횟수(turn)을 연료로 추상화
+ */
 public class Gas {
 
-    public static final int MIN_GAUGE = 0;
-    public static final int MAX_GAUGE = 100;
-    public static final int CONSUME_AMOUNT = 1;
+    public static final int MIN_GAUGE = 0;  // 최소 수치
+    public static final int MAX_GAUGE = 100; // 임의의 최대 수치
+    public static final int CONSUME_AMOUNT = 1; // 소비량
 
-    private int amount;
+    // 수량
+    private final int amount;
 
-
-    public Gas(int amount) {
+    public Gas(final int amount) {
 
         if (amount < MIN_GAUGE || amount >= MAX_GAUGE)
             throw new IllegalArgumentException();
@@ -23,19 +27,31 @@ public class Gas {
         return amount == gas.amount;
     }
 
-    @Override
-    public String toString() {
-        return String.valueOf(amount);
-    }
 
+    /**
+     * 연료 소비 메시지를 처리
+     *
+     * @return 잔여 가스량 반환
+     */
     public Gas consume() {
 
         int remainAmount = this.amount - CONSUME_AMOUNT;
         if (remainAmount < MIN_GAUGE)
             throw new IllegalStateException();
 
-        this.amount = remainAmount;
-        return new Gas(this.amount);
+
+        return new Gas(remainAmount);
     }
 
+    /**
+     * 잔여량이 존재하는지 판단
+     */
+    public boolean has() {
+        return this.amount > MIN_GAUGE;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(amount);
+    }
 }
